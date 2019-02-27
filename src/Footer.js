e87410487b133f02f92d63b64706c4b3f36cc0b7
Will Ward <will.j.ward3@gmail.com>
2019-02-28 02:23:40 +0900

    Added message - Powered by Google

--- src/Footer.js
+++ src/Footer.js
@@ -8,6 +8,7 @@ function Footer() {
     return (
         <div>
             <hr className="mt-5"/>
+            <p className="text-center">Powered by <a href="https://developers.google.com/books/">Google Books</a></p>
             <p className="text-center mt-5"><FontAwesomeIcon icon={faCopyright} /> 2019 willjw3. All Rights Reserved</p>
         </div>
     )
