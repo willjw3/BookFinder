1d089d0972eabad8e51aefad4af0ab79f71e5413
Will Ward <will.j.ward3@gmail.com>
2019-02-28 11:55:05 +0900

    Updates

---
+++ precache-manifest.9b7c0b8b6716e4ced3f9e10895719a55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ab14a012a3ebdd80f5f",
+    "url": "/BookFinder/static/js/runtime~main.9ab14a01.js"
+  },
+  {
+    "revision": "6e856c6ef95d9419ace4",
+    "url": "/BookFinder/static/js/main.6e856c6e.chunk.js"
+  },
+  {
+    "revision": "31ab4d4349abc1c9dd81",
+    "url": "/BookFinder/static/js/2.31ab4d43.chunk.js"
+  },
+  {
+    "revision": "6e856c6ef95d9419ace4",
+    "url": "/BookFinder/static/css/main.382692c5.chunk.css"
+  },
+  {
+    "revision": "afb5968093e24d66c448ca589df5aff1",
+    "url": "/BookFinder/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/BookFinder/precache-manifest.9b7c0b8b6716e4ced3f9e10895719a55.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/BookFinder/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
